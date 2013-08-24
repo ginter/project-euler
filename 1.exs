e0979ac3f255db513ef6a56a8bb9f0b43161e587
Chris Ginter <ginteruf@gmail.com>
2013-08-24 11:46:36 -0500

    refactor project 1

--- 1.exs
+++ 1.exs
@@ -1,7 +1,8 @@
 defmodule Sum do
-  def sum(1000, acc), do: acc
-  def sum(x, acc) when rem(x,5) == 0 or rem(x,3) == 0, do: sum(x+1, acc+x)
-  def sum(x, acc), do: sum(x+1, acc)
+  def sum(x), do: sum(x, 0)
+  def sum(0, acc), do: acc
+  def sum(x, acc) when rem(x,5) == 0 or rem(x,3) == 0, do: sum(x-1, acc+x)
+  def sum(x, acc), do: sum(x-1, acc)
 end
 
-IO.puts Sum.sum(0, 0)
+IO.puts Sum.sum(999)
