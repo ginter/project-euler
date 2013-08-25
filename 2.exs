9555097ef2d7dbd70f31a589b38d95739ab9878f
Chris Ginter <ginteruf@gmail.com>
2013-08-25 15:36:58 -0500

    factor fib into module

--- 2.exs
+++ 2.exs
@@ -1,13 +1,13 @@
-defmodule Fib do
-  def sum(limit), do: do_sum(fib(1), 1, limit)
+Code.require_file "fib.exs"
+
+defmodule EvenFibonacciNumbers do
+  import Fib
+
+  def sum(limit: limit), do: do_sum(fib(1), 1, limit)
 
   def do_sum(fibn, _n, limit) when fibn > limit, do: 0
   def do_sum(fibn, n, limit) when rem(fibn, 2) != 0, do: do_sum(fib(n+1), n+1, limit)
   def do_sum(fibn, n, limit), do: fibn + do_sum(fib(n+1),n+1, limit)
-
-  def fib(0), do: 1
-  def fib(1), do: 1
-  def fib(n), do: fib(n-1) + fib(n-2)
 end
 
-IO.puts Fib.sum(4_000_000)
+IO.puts EvenFibonacciNumbers.sum(limit: 4_000_000)
---
+++ fib.exs
@@ -0,0 +1,6 @@
+defmodule Fib do
+  def fib(0), do: 1
+  def fib(1), do: 1
+  def fib(n), do: fib(n-1) + fib(n-2)
+end
+
