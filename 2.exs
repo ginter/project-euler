1f80c74a00442c7f65caa2b6dd7fdff46115c9f9
Chris Ginter <ginteruf@gmail.com>
2013-08-25 12:27:50 -0500

    refactor to remove conditionals

--- 2.exs
+++ 2.exs
@@ -1,18 +1,13 @@
 defmodule Fib do
+  def sum(limit), do: do_sum(fib(1), 1, limit)
+
+  def do_sum(fibn, _n, limit) when fibn > limit, do: 0
+  def do_sum(fibn, n, limit) when rem(fibn, 2) != 0, do: do_sum(fib(n+1), n+1, limit)
+  def do_sum(fibn, n, limit), do: fibn + do_sum(fib(n+1),n+1, limit)
+
   def fib(0), do: 1
   def fib(1), do: 1
   def fib(n), do: fib(n-1) + fib(n-2)
-
-  def sum(limit), do: do_sum(1, 0, limit)
-
-  def do_sum(n, acc, limit) do
-    fibn = fib(n)
-    if fibn > limit do
-      acc
-    else
-      if rem(fibn, 2) == 0, do: do_sum(n+1, acc+fibn, limit), else: do_sum(n+1, acc, limit)
-    end
-  end
 end
 
 IO.puts Fib.sum(4_000_000)
