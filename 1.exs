9bebbc761067be614d4fcfe8da84098f28f4adb3
Chris Ginter <ginteruf@gmail.com>
2013-08-25 12:18:17 -0500

    remove unnecessary acc

--- 1.exs
+++ 1.exs
@@ -1,9 +1,7 @@
 defmodule Sum do
-  def sum(x), do: do_sum(x, 0)
-
-  def do_sum(0, acc), do: acc
-  def do_sum(x, acc) when rem(x,5) == 0 or rem(x,3) == 0, do: do_sum(x-1, acc+x)
-  def do_sum(x, acc), do: do_sum(x-1, acc)
+  def sum(0), do: 0
+  def sum(x) when rem(x,5) == 0 or rem(x,3) == 0, do: x + sum(x-1)
+  def sum(x), do: sum(x-1)
 end
 
 IO.puts Sum.sum(999)
