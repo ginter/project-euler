92fc1384b16575d9efc7e535ef9696435249778d
Chris Ginter <ginteruf@gmail.com>
2013-08-25 12:20:04 -0500

    flip cases

--- 1.exs
+++ 1.exs
@@ -1,7 +1,7 @@
 defmodule Sum do
   def sum(0), do: 0
-  def sum(x) when rem(x,5) == 0 or rem(x,3) == 0, do: x + sum(x-1)
-  def sum(x), do: sum(x-1)
+  def sum(x) when rem(x,5) != 0 and rem(x,3) != 0, do: sum(x-1)
+  def sum(x), do: x + sum(x-1)
 end
 
 IO.puts Sum.sum(999)
